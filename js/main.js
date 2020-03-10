ca581f296d6bac81a7169b6d764b302d25a29f60
pb0126 <prabhubisht0126@gmail.com>
2020-03-10 17:03:24 +0530

    backend added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,63 @@
-alert("Hello World!")
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAKZT2u245phtsgSQ5MMXMlNmp4PdP0GgY",
+    authDomain: "actrience-2eea4.firebaseapp.com",
+    databaseURL: "https://actrience-2eea4.firebaseio.com",
+    projectId: "actrience-2eea4",
+    storageBucket: "actrience-2eea4.appspot.com",
+    messagingSenderId: "796169791510",
+    appId: "1:796169791510:web:28012343f8a92fdb382d37",
+    measurementId: "G-EDFDHF68NC"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Refrence messages
+var messageRef = firebase.database().ref('messages');
+
+// Contact form submission
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+
+    // Get values
+
+    var name = getInputVal('name');
+    var email = getInputVal('email');
+    var contact = getInputVal('form-contact');
+    var message = getInputVal('message');
+
+    saveMessage(name, email, contact, message);
+
+    // Show Alert
+    document.querySelector('.alert').style.display = 'block';
+
+    // Time out
+    setTimeout(function(){
+        document.querySelector('.alert').style.display = 'none';
+    }, 1500);
+
+    // Reset
+    document.getElementById('contactForm').reset();
+
+}
+
+// Function to get form values
+
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
+
+// Save messages to firebase
+function saveMessage(name, email, contact, message){
+    var newMessageRef = messageRef.push();
+    newMessageRef.set({
+        name: name,
+        email: email,
+        contact: contact,
+        message: message
+    });
+}
